bc7cb7a6d5eeaddbd19ae00a1e20e2abf9675ac6
kirill555525 <surskillwill@gmail.com>
2024-08-20 02:37:06 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {};
+  for (let arr of bracketsConfig) {
+    obj[arr[0]] = arr[1];
+  }
+
+  let new_str = '';
+
+  for (let i = 0; i < str.length; i++) {
+    new_str += str[i];
+    if (!Object.values(obj).includes(str[i])) continue;
+
+    if (str[i] === obj[new_str.at(-2)]) {
+      new_str = new_str.slice(0, -2);
+      continue;
+    }
+
+    if (Object.keys(obj).includes(str[i])) continue;
+
+    break;
+
+  }
+  
+  return new_str === '';
+
 }
